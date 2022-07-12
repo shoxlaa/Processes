28bed63bbfa280dee3b6d8fc0024c06cb3520a4c
shoxlaa <shahla.guliyeva02@gmail.com>
2022-07-12 18:07:40 +0400

    Добавьте файлы проекта.

---
+++ Processes/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//var processes = Process.GetProcesses();
+
+namespace Processes
+{
+    [INotifyPropertyChanged]
+    public  partial class MainViewModel
+    {
+        Process[] processes = Process.GetProcesses();
+
+        [ObservableProperty]
+        private ObservableCollection<Process>? _processes;
+
+        public MainViewModel()
+        {
+            Processes = new ObservableCollection<Process>(); 
+            foreach(var v in processes)
+            {
+                Processes.Add(v);
+            }
+        }
+
+    }
+}
